6aa58de007f305b877f6bc3a335f774ad2c418f6
sumuyzzz <sumuyzzz@gmail.com>
2021-02-18 13:45:09 +0800

    init

---
+++ src/app1.js
@@ -0,0 +1,30 @@
+import $ from "jquery";
+import "./app1.css";
+
+const $button1 = $("#add1");
+const $button2 = $("#minus1");
+const $button3 = $("#multiple2");
+const $button4 = $("#division2");
+const $number = $("#number");
+const n = localStorage.getItem("n");
+$number.text(n || 1);
+$button1.on("click", () => {
+  let n = parseInt($number.text());
+  n += 1;
+  $number.text(n);
+});
+$button2.on("click", () => {
+  let n = parseInt($number.text());
+  n -= 1;
+  $number.text(n);
+});
+$button3.on("click", () => {
+  let n = parseInt($number.text());
+  n *= 2;
+  $number.text(n);
+});
+$button4.on("click", () => {
+  let n = parseInt($number.text());
+  n /= 2;
+  $number.text(n);
+});
