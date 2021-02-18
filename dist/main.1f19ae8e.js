477a2457bbc6e938660d38313a597c0292e19fc8
sumuyzzz <sumuyzzz@gmail.com>
2021-02-18 13:53:44 +0800

    init

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -11423,7 +11423,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11756" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2037" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
